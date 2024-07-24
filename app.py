fced309f6452c7ecd5728ae0415a04e568de1fa3
dhertzoficial <daniucs@gmail.com>
2024-07-24 17:40:54 +0100

    1740

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+# API RESTfull
+
 from flask import Flask, request, jsonify
 import sqlite3
 
-app = Flask(__name__)
+# INICIALIZAÇÃO DA APLICAÇÃO FLASK
+app = Flask(__name__) # CRIA UMA INSTÂNCIA DA CLASSE FLASK
 
 def to_connect():
-    connection = sqlite3.connect("database.db")
-    cursor = connection.cursor()
+    connection = sqlite3.connect("database.db") # CRIA CONEXÃO COM DATABASE
+    cursor = connection.cursor() # CURSOS O QUAL É USADO PARA EXECUTAR COMANDOS SQL NO DATABASE
     return connection, cursor
 
-@app.route('/users', methods=['GET'])
+# ENDPOINT PARA OBTER USUÁRIOS
+@app.route('/users', methods=['GET']) # DEFINE A ROTA PARA O ENDPOINT - ACEITA MÉTODO GET
 def get_users():
     connection, cursor = to_connect()
     cursor.execute("SELECT * FROM users")
@@ -16,9 +20,10 @@ def get_users():
     connection.close()
     return jsonify(users)
 
+# ENDPOINT TO ADD USERS
 @app.route('/users', methods=['POST'])
 def add_user():
-    data = request.get_json()
+    data = request.get_json() # OBTEM DADOS ENVIADOS EM UM DICIO PYTHON CONVERTENDO PARA FORMATO JSON
     connection, cursor = to_connect()
     cursor.execute('INSERT INTO users (email, password, name, status) VALUES (?, ?, ?, ?)',
                    (data['email'], data['password'], data['name'], data['status']))
@@ -26,19 +31,43 @@ def add_user():
     connection.close()
     return jsonify({'message': 'User added successfully'})
 
+
+# ENDPOINT TO UPDATE USERS
 @app.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
     data = request.get_json()
     connection, cursor = to_connect()
-    cursor.execute('''
-        UPDATE users
-        SET email = ?, password = ?, name = ?, status = ?
-        WHERE id = ?
-    ''', (data['email'], data['password'], data['name'], data['status'], id))
+
+    # VERIFICA QUAIS CAMPOS FORAM ENVIADOS NO JSON E ATUALIZA SOMENTE OS MESMOS
+
+    fields = []
+    values = []
+
+    if 'email' in data:
+        fields.append('email = ?')
+        values.append(data['email'])
+
+    if 'password' in data:
+        fields.append('password = ?')
+        values.append(data['password'])
+
+    if 'name' in data:
+        fields.append('name = ?')
+        values.append(data['name'])
+    
+    if 'status' in data:
+        fields.append('status = ?')
+        values.append(data['status'])
+
+    values.append(id)
+
+    cursor.execute(f"UPDATE users SET {', '.join(fields)} WHERE id = ?", values)        
+
     connection.commit()
     connection.close()
     return jsonify({'message': 'User updated successfully'})
 
+# ENDPOINT TO DELETE USERS
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
     connection, cursor = to_connect()
@@ -47,7 +76,18 @@ def delete_user(id):
     connection.close()
     return jsonify({'message': 'User deleted successfully'})
 
+# ENDPOINT TO HEALTH CHECK
+@app.route('/health', methods=['GET'])
+def health_check():
+    try:
+        connection, cursor = to_connect()
+        cursor.execute('SELECT 1')
+        connection.close()
+        return jsonify({'status': 'Database connection is active'}), 200
+    except sqlite3.Error as e:
+        return jsonify({'status': 'Database connection faild'}), 500
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # INICIA O SERVIDOR EM MODO DEPURAÇÃO (FORNECE DETALHES DE ERROS E REINICIA AUTO QUANDO CÓDIGO ALTERADO)
 
 
